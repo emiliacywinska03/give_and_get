a9885e653ebd0d9d91b68b64c64f208de4a0e6b1
emiliacywinska03 <emiliacywinskaWS@gmail.com>
2025-05-14 13:06:56 +0200

    Poprawiono LoginRegister.tsx

--- frontend/src/pages/LoginRegister.tsx
+++ frontend/src/pages/LoginRegister.tsx
@@ -23,21 +23,46 @@ const LoginRegister: React.FC = () => {
             {activeTab === 'login'?(
                 <form className="auth-form">
                     <h2>Zaloguj się</h2>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Hasło"/>
+                    <label>
+                        Email lub nazwa użytkownika
+                        <input type="text" name="login" required/>
+                    </label>
+                    <label>
+                        Hasło
+                        <input type="password" name="password" required/>
+                    </label>
                     <button type="submit">Zaloguj się</button>
                 </form>
             ):(
-                <form>
-                    <form className="auth-form">
+                <form className="auth-form">
                     <h2>Zarejestruj się</h2>
-                    <input type="text" placeholder="Nazwa użytkownika"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Hasło"/>
-                    <input type="password" placeholder="Potwierdź hasło"/>
+                    <label>
+                        Nazwa użytkownika
+                        <input type="text" name="username" required/>
+                    </label>
+                    <label>
+                        Imię
+                        <input type="text" name="name"/>
+                    </label>
+                    <label>
+                        Nazwisko
+                        <input type="text" name="surname"/>
+                    </label>
+                    <label>
+                        E-mail
+                        <input type="email" name="email" required/>
+                    </label>
+                    <label>
+                        Hasło
+                        <input type="password" name="password" required/>
+                    </label>
+                    <label>
+                        Potwierdź hasło
+                        <input type="password" name="confirmPassword" required/>
+                    </label>
                     <button type="submit">Zarejestruj się</button>
                 </form>
-                </form>
+                
             )}
         </div>
     );
